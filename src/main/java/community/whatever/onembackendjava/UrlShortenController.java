d287f68a3044741fe7b35ddb419aa8a183302b2d
EunHyeSeon <philo0512@gmail.com>
2025-03-01 22:22:39 +0900

    feat: 로그 부분 추가 - 로그 기능을 위해 롬북 라이브러리 추가 - 서비스 별 요청과 응답 값에 로그 추가 - search 기능 GET method로 변경

--- src/main/java/community/whatever/onembackendjava/UrlShortenController.java
+++ src/main/java/community/whatever/onembackendjava/UrlShortenController.java
@@ -1,5 +1,8 @@
 package community.whatever.onembackendjava;
 
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,23 +11,30 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+
+@Slf4j
 @RestController
 public class UrlShortenController {
 
     private final Map<String, String> shortenUrls = new HashMap<>();
 
-    @PostMapping("/shorten-url/search")
+    @GetMapping("/shorten-url/search")
     public String shortenUrlSearch(@RequestBody String key) {
+        log.info("search key >> {}", key);
         if (!shortenUrls.containsKey(key)) {
             throw new IllegalArgumentException("Invalid key");
         }
-        return shortenUrls.get(key);
+        String originUrl = shortenUrls.get(key);
+        log.info("originUrl >> {}", originUrl);
+        return originUrl;
     }
 
     @PostMapping("/shorten-url/create")
-    public String shortenUrlCreate(@RequestBody String originUrl) {
+    public String shortenUrlCreate( @Valid String originUrl) {
+        log.info("create url >> {}", originUrl);
         String randomKey = String.valueOf(new Random().nextInt(10000));
         shortenUrls.put(randomKey, originUrl);
+        log.info("randomKey >> {}", randomKey);
         return randomKey;
     }
 }
