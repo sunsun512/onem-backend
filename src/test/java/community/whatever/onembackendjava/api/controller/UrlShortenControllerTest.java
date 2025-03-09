cdd2ac431ea3fb8cf990c7df42d88fb87ac35dd4
EunHyeSeon <philo0512@gmail.com>
2025-03-09 22:01:35 +0900

    feat: - pull refactoring

--- src/test/java/community/whatever/onembackendjava/api/controller/UrlShortenControllerTest.java
+++ src/test/java/community/whatever/onembackendjava/api/controller/UrlShortenControllerTest.java
@@ -1,13 +1,18 @@
 package community.whatever.onembackendjava.api.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import community.whatever.onembackendjava.api.dto.ShortenUrlDto;
 import community.whatever.onembackendjava.setting.RestDocsTestSupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
@@ -24,12 +29,19 @@ class UrlShortenControllerTest  extends RestDocsTestSupport {
 
     @Test
     void shortenUrlSearch() throws Exception{
+        ShortenUrlDto.Get.Request dto = new ShortenUrlDto.Get.Request();
+        dto.setKey("11111111");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        Map<String, String> map = objectMapper.convertValue(dto, new TypeReference<Map<String, String>>() {
+        });
+        params.setAll(map);
 
         ResultActions result = this.mockMvc.perform(
                 get("/shorten-url")
                         .contentType(contentType)
                         .accept(contentType)
-                        .queryParam("key", "111111")
+                        .params(params)
         );
 
         result.andExpect(status().isOk())
@@ -52,15 +64,14 @@ class UrlShortenControllerTest  extends RestDocsTestSupport {
 
     @Test
     void shortenUrlCreate() throws Exception {
-        HashMap<String , String> req = new HashMap<>();
-        req.put("originUrl", "https://www.google.com");
-//        req.put("originUrl", " ");
+        ShortenUrlDto.Create.Request dto  = new ShortenUrlDto.Create.Request();
+        dto.setOriginUrl("https://www.google.com");
 
         ResultActions result = this.mockMvc.perform(
                 post("/shorten-url")
                         .contentType(contentType)
                         .accept(contentType)
-                        .content(objectMapper.writeValueAsString(req))
+                        .content(objectMapper.writeValueAsString(dto))
         );
 
         result.andExpect(status().isOk())
@@ -68,16 +79,15 @@ class UrlShortenControllerTest  extends RestDocsTestSupport {
                 .andDo(restDocs.document(
                                 requestFields(
                                         fieldWithPath("originUrl").type(JsonFieldType.STRING).description("요청 url")
+                                ),
+                                responseFields(
+                                        fieldWithPath("resultCode").type(JsonFieldType.STRING).description("결과 코드"),
+                                        fieldWithPath("msg").type(JsonFieldType.STRING).description("결과 메시지"),
+                                        fieldWithPath("timeZone").type(JsonFieldType.STRING).description("서버 타임존"),
+                                        fieldWithPath("timeStamp").type(JsonFieldType.NUMBER).description("서버 응답시간"),
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 객체").optional(),
+                                        fieldWithPath("data.key").type(JsonFieldType.STRING).description("생성된 키").optional()
                                 )
-//                                responseFields(
-////                                        fieldWithPath("resultCode").type(JsonFieldType.STRING).description("결과 코드"),
-////                                        fieldWithPath("msg").type(JsonFieldType.STRING).description("결과 메시지"),
-////                                        fieldWithPath("timeZone").type(JsonFieldType.STRING).description("서버 타임존"),
-////                                        fieldWithPath("timeStamp").type(JsonFieldType.NUMBER).description("서버 응답시간"),
-////                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 객체").optional(),
-////                                        fieldWithPath("data.key").type(JsonFieldType.STRING).description("생성된 키").optional()
-//                                        fieldWithPath("").type(JsonFieldType.NUMBER).description("생성된 키").optional()
-//                                )
                         )
                 );
     }
