740e53d0290e41c40315ef642c2f77f55b0a7a07
EunHyeSeon <philo0512@gmail.com>
2025-03-25 23:08:53 +0900

    feat: - unused import 삭제

--- src/main/java/community/whatever/onembackendjava/api/service/UrlShortenService.java
+++ src/main/java/community/whatever/onembackendjava/api/service/UrlShortenService.java
@@ -8,7 +8,6 @@ import community.whatever.onembackendjava.common.error.model.ErrorCode;
 import community.whatever.onembackendjava.common.util.GetEnvironmentPrefix;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
